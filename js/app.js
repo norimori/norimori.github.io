ed742aa34aad89d1914115a8b489a49ad719541e
norimori <norimori@uw.edu>
2015-09-15 10:30:27 -0700

    Push all the things

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*
+    app.js: main application script
+    this is an Angular application
+ */
+
+"use strict";
+
+//create new angular module
+//args = name of module, modules that this module depends on
+angular.module('MovieApp', [])
+	.controller('MoviesController', function($scope) {
+		$scope.movies = movies;
+		$scope.searchString = '';
+		$scope.sortCol = 'rank';
+		$scope.sortReverse = false;
+
+		$scope.sortBy = function (colName) {
+			if ($scope.sortCol == colName) {
+				$scope.sortReverse = !$scope.sortReverse;
+			} else {
+				$scope.sortReverse = false;
+			}
+			$scope.sortCol = colName;
+		};
+	});

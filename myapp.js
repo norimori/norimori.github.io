401aec2369696ba39611b65ed127211ad5fe2668
norimori <norimori@uw.edu>
2015-09-16 11:56:54 -0700

    list

--- myapp.js
+++ myapp.js
@@ -2,9 +2,25 @@ var app = angular.module('flapperNewsYAY', []);
 
 //Creating controller
 app.controller('MainCtrl', [
-	'$scope',
+	'$scope', //Initialize $scope variable
+
+	/*
+		$scope variable serves as the bridge between Angular controllers
+		and Angular templates. If soemthing needs to be accessible in the
+		template, like a function/variable, bind it to $scope.
+	*/
 	function($scope) {
-		$scope.testHUH = 'Hello world! HUH?!';
+		//$scope variable defines a string
+		$scope.testYAY = 'Hello world! HUH?!';
+
+		//$scope variable defines a list of post titles
+		$scope.posts = [
+			'post 1',
+			'post 2',
+			'post 3',
+			'post 4',
+			'post 5',
+		];
 	}
 ]);
 

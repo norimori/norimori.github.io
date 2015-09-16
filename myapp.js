38efb650d6e85bd04aced7dccd31e0a217026952
norimori <norimori@uw.edu>
2015-09-16 11:43:07 -0700

    maybe

--- myapp.js
+++ myapp.js
@@ -4,7 +4,8 @@ var app = angular.module('flapperNewsYAY', []);
 app.controller('MainCtrl', [
 	'$scope',
 	function($scope) {
-		$scope.test = 'Hello world! Please work? \n Does this escape key work?';
+		$scope.test = 'Hello world! Please work? 
+		\n Does this escape key work? Maybe?';
 	}
 ]);
 

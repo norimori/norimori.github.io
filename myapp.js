90554e28c7bbade049010e82d3efc618f0835a28
norimori <norimori@uw.edu>
2015-09-16 15:12:20 -0700

    fix

--- myapp.js
+++ myapp.js
@@ -22,36 +22,39 @@ app.controller('MainCtrl', [
 			{title: 'post 4', upvotes: 9},
 			{title: 'post 5', upvotes: 4}
 		];
+
+		//Allows users to add posts. 
+		/*Adds function to $scope variable that adds an object into the
+		'posts' array. When this function is called, it appends a new post to
+		the $scope.posts variable. */
+		$scope.addPost = function() {
+
+			/*
+				So, when we add a post we're getting the title from $scope.title,
+				which we then clear after the post has been created, to prevent
+				the user from posting a blank title.
+			*/
+			if (!$scope.title || $scope.title === '') {
+				return;
+			}
+
+			//push appends a variable
+			/*
+				addPost function retrieves the title entered into the form
+				bound to the $scope variable 'title', and set 'title' to
+				blank once it's been added to the posts array.
+			*/
+			$scope.posts.push({title: 'A new post!', upvotes: 0});
+
+
+
+			//Resets contents of $scope.title to blank string.
+			$scope.title = '';
+		};
+
+
 	}
 
-	//Allows users to add posts. 
-	/*Adds function to $scope variable that adds an object into the
-	'posts' array. When this function is called, it appends a new post to
-	the $scope.posts variable. */
-	$scope.addPost = function() {
-
-		/*
-			So, when we add a post we're getting the title from $scope.title,
-			which we then clear after the post has been created, to prevent
-			the user from posting a blank title.
-		*/
-		if (!$scope.title || $scope.title === '') {
-			return;
-		}
-
-		//push appends a variable
-		/*
-			addPost function retrieves the title entered into the form
-			bound to the $scope variable 'title', and set 'title' to
-			blank once it's been added to the posts array.
-		*/
-		$scope.posts.push({title: 'A new post!', upvotes: 0});
-
-
-
-		//Resets contents of $scope.title to blank string.
-		$scope.title = '';
-	};
 
 ]);
 
